00a978ce260c40562ff16d61f9facba918b35827
Sergey Puliaiev <s.puliaiev@gmail.com>
2017-06-16 18:21:02 +0300

    Initial Commit

---
+++ LispMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LispMac
+//
+//  Created by Sergii Puliaiev on 6/16/17.
+//  Copyright © 2017 Sergii Puliaiev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LispMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LispMac
+//
+//  Created by Sergii Puliaiev on 6/16/17.
+//  Copyright © 2017 Sergii Puliaiev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
