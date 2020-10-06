269931c2b8b8996a695c1bf987e015aede0b1e9d
yonihemi <jonathan@hemi.dev>
2020-10-07 00:22:08 +0800

    Update to Xcode 12/Swift 5.3/SPM (#11)

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LispCore",
+    platforms: [.macOS(.v10_15), .iOS(.v9)],
+    products: [
+        .library(name: "LispCore", targets: ["LispCore"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LispCore",
+            resources: [
+                Resource.copy("Resources/eval.lisp"),
+            ]),
+        .testTarget(name: "LispCoreTests",
+                dependencies: ["LispCore"])
+    ]
+)
